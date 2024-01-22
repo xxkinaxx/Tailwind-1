9f6f5a35d1a679ec3cec362f06eb51cc24b76fa4
xxkinaxx <ekopandu088@gmail.com>
2024-01-22 14:17:54 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      spacing: {
+        '13':'3.25rem'
+      }
+    },
+  },
+  plugins: [],
+}
+
